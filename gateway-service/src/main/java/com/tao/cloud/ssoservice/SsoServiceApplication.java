f4e688886a8b23df0c97377ba149b8ced8ed9afc
Your Name <you@example.com>
2021-06-11 23:52:26 +0800

    add gateway module

--- gateway-service/src/main/java/com/tao/cloud/ssoservice/SsoServiceApplication.java
+++ gateway-service/src/main/java/com/tao/cloud/ssoservice/SsoServiceApplication.java
@@ -2,8 +2,10 @@ package com.tao.cloud.ssoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SsoServiceApplication {
 
     public static void main(String[] args) {
